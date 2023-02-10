abb5bee36185b2d500c02b30445e0ebcd13fca77
Mateusz <mateusz.adamczyk@monterail.com>
2023-02-10 14:14:14 +0100

    Use dynamic module federation for remoteApp

--- projects/host-app/src/app/app-routing.module.ts
+++ projects/host-app/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { loadRemoteModule } from '@angular-architects/module-federation';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('remoteApp/InnerModule').then((m) => m.InnerModule),
+    loadChildren: () =>
+      loadRemoteModule({
+        type: 'module',
+        remoteEntry: 'http://localhost:4201/remoteEntry.js',
+        exposedModule: './InnerModule',
+      }).then((m) => m.InnerModule),
   },
 ];
 
